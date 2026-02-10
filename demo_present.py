8899f5b92d818393d3d1b067bc7903d049b0ec39
OnepireMS <160677919+OnepireMS@users.noreply.github.com>
2026-02-10 06:19:16 +0000

    new item added

--- demo_present.py
+++ demo_present.py
@@ -4,11 +4,12 @@ import time
 
 mc = MyCobot(PI_PORT, PI_BAUD)
 speed = 20
-mode = 1  
+mode = 1 
+z = 140 
 
-cube    = [180, 150, 140, -180, 0, -45]
-d20     = [200, 120, 140, -180, 0, -45]
-battery = [220,  90, 140, -180, 0, -45]
+cube    = [180, 150, 95, -180, 0, -45]
+d20     = [200, 120, 95, -180, 0, -45]
+battery = [220,  90, 95, -180, 0, -45]
 
 mc.send_angles([0, 0, 0, 0, 0, 0], speed)
 time.sleep(4)
@@ -24,6 +25,40 @@ time.sleep(1)
 
 print("POT:", pot_coords)
 
+def at_safe_z(p):
+    return [p[0], p[1], z, p[3], p[4], p[5]]
+
+def pick_and_place(obj, pot):
+    
+    mc.send_coords(at_safe_z(pot), speed, mode)
+    time.sleep(4)
+
+    
+    mc.send_coords(at_safe_z(obj), speed, mode)
+    time.sleep(5)
+
+    
+    mc.set_gripper_value(70, 25)   
+    time.sleep(1)
+    mc.send_coords(obj, speed, mode)
+    time.sleep(4)
+    mc.set_gripper_value(10, 25)    
+    time.sleep(2)
+    mc.send_coords(at_safe_z(obj), speed, mode)
+    time.sleep(4)
+
+    
+    mc.send_coords(at_safe_z(pot), speed, mode)
+    time.sleep(5)
+
+    
+    mc.send_coords(pot, speed, mode)
+    time.sleep(4)
+    mc.set_gripper_value(70, 25)   
+    time.sleep(2)
+    mc.send_coords(at_safe_z(pot), speed, mode)
+    time.sleep(4)
+
 while True:
     print("\n1 = Cube\n2 = D20\n3 = Battery\n0 = Stop")
     choice = input("Sort: ").strip()
@@ -31,26 +66,11 @@ while True:
     if choice == "0":
         mc.stop()
         break
-
-    if choice == "1":
-        obj = cube
+    elif choice == "1":
+        pick_and_place(cube, pot_coords)
     elif choice == "2":
-        obj = d20
+        pick_and_place(d20, pot_coords)
     elif choice == "3":
-        obj = battery
+        pick_and_place(battery, pot_coords)
     else:
         print("Invalid.")
-        continue
-    
-    mc.set_gripper_value(70, 25)
-    time.sleep(0.5)
-    
-    mc.send_coords(obj, speed, mode)
-    time.sleep(5)
-    mc.set_gripper_value(10, 25)
-    time.sleep(2)
-    
-    mc.send_coords(pot_coords, speed, mode)
-    time.sleep(5)
-    mc.set_gripper_value(70, 25)
-    time.sleep(2)
